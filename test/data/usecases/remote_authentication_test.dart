a25b739c0e57a827c7655e05b11cd4a4213bcbab
Rodrigo Avelino <rodrigo.g.avelino@gmail.com>
2021-04-30 22:14:48 -0300

    ensure RemoteAuthentication throws UnexpectedError on 401

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -112,15 +112,16 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
-  test('Should throw UnexpectedError if HttpClient returns 500', () async {
+  test('Should throw InvalidCredentialsError if HttpClient returns 401',
+      () async {
     when(httpClient.request(
             url: anyNamed('url'),
             method: anyNamed('method'),
             body: anyNamed('body')))
-        .thenThrow(HttpError.serverError);
+        .thenThrow(HttpError.unauthorized);
 
     final future = sut.auth(params);
 
-    expect(future, throwsA(DomainError.unexpected));
+    expect(future, throwsA(DomainError.invalidCredentials));
   });
 }
